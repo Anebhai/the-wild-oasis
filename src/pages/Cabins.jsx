85c5dff5a2159e9fb238fd51d79b2bf3949ebbb6
Anebhai <ane.iqbal@gmail.com>
2023-10-19 17:01:58 +0530

    supabase setting done

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(function () {
+    getCabins().then((data) => console.log(data));
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://niuwunfutjylyntkardg.supabase.co/storage/v1/object/public/cabin-images/cabin-003.jpg"
+        alt=""
+      />
     </Row>
   );
 }
